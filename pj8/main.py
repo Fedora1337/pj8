6ace113e86c4423daf0ca559dae9f00a3fa66fe6
Fedora1337 <choccholanghe@gmail.com>
2025-08-08 21:19:24 +0700

    Update main.py

--- pj8/main.py
+++ pj8/main.py
@@ -8,6 +8,9 @@ with st.sidebar:
     st.write('Trần Trương Minh Vương (sinh ngày 21 tháng 12 năm 1984), thường được biết tới đơn giản là Minh Vương (hay còn gọi là Minh Vương M4U) là nam ca sĩ và nhạc sĩ nhạc trẻ của Việt Nam, có sở trường về nhạc pop ballad.')
 st.title('Bài hát yêu thích')
 st.write('Trúc xinh (Remix)')
+audio=open('Trucxinh.mp3','rb')
 st.audio('Trucxinh.mp3',format='mp3')
 st.title('MV yêu thích')
-st.video('https://www.youtube.com/watch?v=EHASipZkZnk&list=RDEHASipZkZnk&start_radio=1&pp=ygURZW0gxqFpIGzDqm4gcGjhu5GgBwE%3D',format='mp4/video')
+video='https://www.youtube.com/watch?v=EHASipZkZnk&list=RDEHASipZkZnk&start_radio=1&pp=ygURZW0gxqFpIGzDqm4gcGjhu5GgBwE%3D'
+st.video(video,format='mp4/video')
+
